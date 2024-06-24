26bedf1aade39629fd22760417a4ace39f80b94c
elliott <elliott>
2024-06-24 13:34:08 +0300

    Initial commit

---
+++ WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Мадина Валиева on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
